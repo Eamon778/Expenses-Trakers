409f3edbeca53037b077f346270b3cd38b86df0c
Eamon Ali <eamonmondol525@gmail.com>
2024-12-28 20:23:57 +0600

    somethng

--- server/app.js
+++ server/app.js
@@ -1,1 +1,28 @@
- 
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const cors = require('cors')
+const productRouter = require('./routes/productRouter')
+const authRouter = require('./routes/authRouter')
+const { ensureAuth } = require('./middleware/auth')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use('/api/auth', authRouter)
+app.use('/api/tracker', ensureAuth, productRouter)
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port: ${process.env.PORT}`)
+    })
+  } catch (error) {
+    console.error('Something went wrong', error)
+  }
+}
+
+start()
